fb72d480fe5fb71449ad2cee0f32090767971787
Jack Carpenter <lynxster222@gmail.com>
2016-08-22 11:36:59 -0400

    mon 8/22/16
    lots of good stuff. questions with Hart

---
+++ js/project.js
@@ -0,0 +1,82 @@
+/*
+$(document).ready(function(){
+	$(".body").mouseover(function(){
+		$(".merg").animate({
+			color : blue;
+		})
+	});
+	
+});  
+*/
+$(document).ready(function(){
+    $(".rate").hover(function(){
+        $(this).css("background-color", "yellow");
+				}, function(){
+        $(this).css("background-color", "pink");
+    });
+});
+
+/*
+
+$(document).ready(function(){
+	$("body").mouseenter(function() {
+		$(".tagline").fadeIn(2000);
+	});
+	$("body").mouseenter(function() {
+		$(".tagline").fadeOut(5000);
+	});
+
+//	$("#toiletSound").click(function(){
+//		var $audio = $('#audio');
+//		var audio = document.getElementById("audio");
+//	
+//		console.log($audio);
+//		console.log('-------------');
+//		console.log(audio);
+//
+//		audio.play();
+//	});
+	$("body").mouseenter(function(){
+		var $audio = $('#audio');
+		var audio = document.getElementById("audio");
+		
+		//console.log($audio);
+		console.log('-------------');
+		console.log(audio);
+		audio.play();
+	});
+	
+//All I seem to need is the AUDIO PLAY in next line & html line 65.thats all!
+//		audio.play();	
+	$(".toiletRepairVideo").click(function(){
+		video.play();
+	});
+
+	$("body").hover(function(){
+		$(".merg").fadeIn(1000);
+	});
+	$("body").hover(function(){
+		$(".merg").fadeOut(1000);
+	});
+//animate colors with BitStorm.com...applying to index.html .blurb
+	$(".blurb").onclick(function(){
+	$('.blurb').animate({backgroundColor: '#400101'});
+	});
+
+
+});
+
+ function calculateSquareHorFootage() {
+ 	var footLength = document.getElementById("footLength").value;
+ 	var footWidth = document.getElementById("footWidth").value;
+	var calculateSquareHorFootage = footWidth * footLength;
+ 	document.getElementById("sqFootage").innerHTML = calculateSquareHorFootage;
+ }
+	document.getElementById("clickme").onclick = calculateSquareHorFootage;
+
+
+*/
+
+
+
+

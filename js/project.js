829fd6247cf95104e5fc7a23cbd00bf9e4fccaca
Jack Carpenter <lynxster222@gmail.com>
2016-08-25 09:52:57 -0400

    8/25/16
    Some corrections, changes. BUT not able to download the form I created
    in Google Forms…says i need to get permission from the owner.

--- js/project.js
+++ js/project.js
@@ -1,85 +1,71 @@
-/*
 $(document).ready(function(){
-	$(".body").mouseover(function(){
-		$(".merg").animate({
-			color : blue;
-		})
-	});
 
-});
-*/
-$(document).ready(function(){
-    $(".rate").hover(function(){
-        $(this).css("background-color", "yellow");
-				}, function(){
-        $(this).css("background-color", "pink");
-    });
-    $('#hamburger').click(function(event) {
-        event.preventDefault();
+		$(".callouts").hover(function(){
+			$(".rate").css("color","maroon");
+		});
+
+    $("div").css("border-radius","5px");
+ 
+
+	$('#hamburger').click(function(event) {
+        	event.preventDefault();
         $('nav').toggleClass('visible');
     });
 
-    $('.gallery').featherlightGallery();
-});
-
-
-/*
-
-$(document).ready(function(){
 	$("body").mouseenter(function() {
 		$(".tagline").fadeIn(2000);
 	});
+
 	$("body").mouseenter(function() {
 		$(".tagline").fadeOut(5000);
 	});
-
-//	$("#toiletSound").click(function(){
-//		var $audio = $('#audio');
-//		var audio = document.getElementById("audio");
-//
-//		console.log($audio);
-//		console.log('-------------');
-//		console.log(audio);
-//
-//		audio.play();
-//	});
+//*	
+//*TOILET FLUSH					
+//*
 	$("body").mouseenter(function(){
 		var $audio = $('#audio');
 		var audio = document.getElementById("audio");
-
-		//console.log($audio);
-		console.log('-------------');
-		console.log(audio);
 		audio.play();
 	});
-
-//All I seem to need is the AUDIO PLAY in next line & html line 65.thats all!
-//		audio.play();
+		
+//*
+//*REPAIR VIDEOS
 	$(".toiletRepairVideo").click(function(){
 		video.play();
 	});
-
-	$("body").hover(function(){
-		$(".merg").fadeIn(1000);
-	});
-	$("body").hover(function(){
-		$(".merg").fadeOut(1000);
-	});
-//animate colors with BitStorm.com...applying to index.html .blurb
-	$(".blurb").onclick(function(){
-	$('.blurb').animate({backgroundColor: '#400101'});
-	});
-
-
 });
+//*
+//*CALCULATE SQUARE FOOTAGE FOR CLEANING
+//*
+document.getElementById("clickme").onclick = calculateSquareHorFootage;
 
  function calculateSquareHorFootage() {
  	var footLength = document.getElementById("footLength").value;
  	var footWidth = document.getElementById("footWidth").value;
 	var calculateSquareHorFootage = footWidth * footLength;
  	document.getElementById("sqFootage").innerHTML = calculateSquareHorFootage;
- }
-	document.getElementById("clickme").onclick = calculateSquareHorFootage;
+}
 
 
-*/
+//LEAVING IN FOR AN EXAMPLE OF CONSOLE.LOG USAGE
+					//	$("#toiletSound").click(function(){
+					//		var $audio = $('#audio');
+					//		var audio = document.getElementById("audio");
+					//
+					//		console.log($audio);
+					//		console.log('-------------');
+					//		console.log(audio);
+					//
+					//		audio.play();
+					//	});	
+
+					//console.log($audio);
+					//	console.log('-------------');
+					//	console.log(audio);
+					//	audio.play();
+					//All I seem to need is the AUDIO PLAY 
+
+					//  Leaving in to remind me to learn about BitStorm
+					//animate colors with BitStorm.com...applying to index.html .blurb
+					//	$(".blurb").onclick(function(){
+					//	$('.blurb').animate({backgroundColor: '#400101'});
